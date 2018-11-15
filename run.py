818bf6b29484869ce9dfd20c2ccf1ac9d711835c
kahartman2 <41496627+kahartman2@users.noreply.github.com>
2018-11-15 18:17:41 -0500

    Add files via upload

---
+++ run.py
@@ -0,0 +1,7 @@
+from dash_package import *
+from dash_package.db_seed import *
+#from dash_package.layout import *
+#from dash_package.etl import *
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
